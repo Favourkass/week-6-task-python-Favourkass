c31f9e42bb5e0b15119a69d0dc19578be14119a7
Decagon <favour.chukwuemeka@decagon.dev>
2021-05-04 00:46:20 +0100

    adding functionalities to books.py

---
+++ models/users.py
@@ -0,0 +1,75 @@
+import psycopg2
+import time
+
+
+
+class Users:
+    def __init__(self):
+        
+        self.conn = psycopg2.connect(host = 'localhost', dbname = 'books_users_db' , user = 'postgres', password = '12345')
+        self.cursor = self.conn.cursor() 
+        # self.cursor.execute('CREATE DATABASE books_users_db')
+       
+        self.conn.autocommit = True
+        
+       
+
+        
+       
+        
+    # def create_connection(self):
+    #     self.cursor.execute('CREATE DATABASE books_users_db')
+    #     self.conn = psycopg2.connect(host = 'localhost', dbname ='books_users_db' , user = 'postgres', password = '12345')
+    #     self.cursor = self.conn.cursor() 
+        
+       
+    #     self.conn.autocommit = True
+        
+
+
+    
+    def create_table(self):
+        with open('sql_files/schema.sql', 'r') as schema_file:
+            schema_values = schema_file.read()
+            self.cursor.execute(schema_values)
+            self.conn.commit()
+            self.conn.close()
+
+
+    def populate_table(self):
+       
+        with open('sql_files/seeder.sql', 'r') as seeder_file:
+            seeder_values = seeder_file.read()
+            self.cursor.execute(seeder_values)
+            self.conn.commit()
+            
+
+    def create_user(self, username, firstname, lastname ):
+        self.cursor.execute('INSERT INTO users(username, firstname, lastname)'
+                            'VALUES(%s,%s, %s)',
+                            (username,firstname,lastname))
+        self.conn.commit()
+
+    def get_all_users(self):
+        self.cursor.execute('SELECT * FROM users')
+        return self.cursor.fetchall()
+
+    def get_user(self, id):
+        self.cursor.execute(f"SELECT * FROM users WHERE id = {id}")
+        return self.cursor.fetchall()
+
+    def update_record_of_user(self, username, firstname, lastname, id):
+        self.cursor.execute(f'UPDATE users SET username = %s, firstname = %s, lastname = %s WHERE id = {id}', (username,firstname,lastname))
+        self.conn.commit()
+    def destroy(self, id):
+        self.cursor.execute(f'DELETE FROM users WHERE id = {id}')
+
+    
+
+    
+
+
+
+values = Users()
+
+print(values.populate_table())
--- student_records/student_records.py
+++ student_records/student_records.py
@@ -37,7 +37,7 @@ class StudentRecord:
         self.connection.commit()
 
     def delete_record(self, ssn):
-        self.cursor.execute('''DELETE FROM student_table WHERE ssn = ?''', (ssn))
+        self.cursor.execute('DELETE FROM student_table WHERE ssn=?', (ssn,))
         self.connection.commit()
 
     def scored_above_50(self):
@@ -53,7 +53,8 @@ class StudentRecord:
 
 
 value = StudentRecord()
-print(value.delete_record(145-53-1634))
+print(value.delete_record('145-53-1634'))
+print(value.read_all())
 # print(value.update_record( '60', '78', '86','64', '80', 'A', '345-67-8901'))
 # print(value.add_student('Long', 'nicholas', '145-53-1634', '40', '50','60', '65','67', 'A' ))
 
