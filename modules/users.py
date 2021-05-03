00ab73efc750328499baa2618053cfcc4d3e9542
Decagon <favour.chukwuemeka@decagon.dev>
2021-05-03 22:18:47 +0100

    starting task 1

--- modules/users.py
+++ modules/users.py
@@ -4,23 +4,23 @@ import time
 
 
 class Users:
-    # def __init__(self):
-       
-
-        
-    def create_database(self):
+    def __init__(self):
         self.conn = psycopg2.connect(host = 'localhost', dbname = None, user = 'postgres', password = '12345') 
         self.cursor = self.conn.cursor() 
         self.conn.autocommit = True
         self.cursor.execute('DROP DATABASE IF EXISTS books_users_db')
         self.cursor.execute('CREATE DATABASE books_users_db')
+       
+
+        
+    # def create_database(self):
+       
         
         
 
 
 
     def create_connection(self):
-        self.create_database()
         self.conn = psycopg2.connect(host = 'localhost', dbname ='books_users_db' , user = 'postgres', password = '12345')
 
         self.cursor = self.conn.cursor()
@@ -34,7 +34,16 @@ class Users:
             self.conn.commit()
 
 
+    def populate_table(self):
+        self.create_connection()
+        with open('sql_files/seeder.sql', 'r') as seeder_file:
+            seeder_values = seeder_file.read()
+            self.cursor.execute(seeder_values)
+            self.conn.commit()
+
+
 
 
 values = Users()
 values.create_table()
+values.populate_table()
